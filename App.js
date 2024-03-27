bdc18768215e89bef1414675226aa7b0d9aad9d6
Uss Dev 1 <ussdev1@unitesoftwaresolutions.com>
2024-03-27 11:14:11 +0530

    Episode 1 using React CDN Link

---
+++ App.js
@@ -0,0 +1,40 @@
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement(
+      "h1",
+      { key: "h1", id: "heading" },
+      "I'm a child1 h1 Tag"
+    ),
+    React.createElement(
+      "h2",
+      { key: "h2", id: "heading" },
+      "I'm a child1 h2 Tag"
+    ),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement(
+      "h1",
+      { key: "h1", id: "heading" },
+      "I'm a child2 h1 Tag"
+    ),
+    React.createElement(
+      "h2",
+      { key: "h2", id: "heading" },
+      "I'm a child2 h2 Tag"
+    ),
+  ]),
+]); // Its an object and this object is a React Element
+
+console.log(parent);
+
+// ReactElement(Object) => HTML(Browser Understands)
+
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading", abc: "abc" },
+//   "Hello World from React !"
+// ); // Its a react element and returns an object
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
