8d69117e5de19bd74b91f58aa62aefce52229c09
Uss Dev 1 <ussdev1@unitesoftwaresolutions.com>
2024-03-29 18:11:54 +0530

    Episode-3 Namaste React Completed

--- App.js
+++ App.js
@@ -1,43 +1,31 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement(
-      "h1",
-      { key: "h1", id: "heading" },
-      "I'm a child1 h1 Tag"
-    ),
-    React.createElement(
-      "h2",
-      { key: "h2", id: "heading" },
-      "I'm a child1 h2 Tag"
-    ),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement(
-      "h1",
-      { key: "h1.", id: "heading" },
-      "I'm a child2 h1 Tag"
-    ),
-    React.createElement(
-      "h2",
-      { key: "h2.", id: "heading" },
-      "I'm a child2 h2 Tag"
-    ),
-  ]),
-]); // Its an object and this object is a React Element
+// JSX - This is not a pure javascript code
+// It is transpiled by babel
+const jsxHeading = (
+  <div>
+    <h1 id="heading">Namaste React using JSX</h1>
+    <h2>Multiple Tags</h2>
+  </div>
+);
 
-console.log(parent);
+// Functional Components
+const TitleComponent = (props) => (
+  <div>
+    <h1>Namaste React {props?.title} Functional Component.</h1>
+  </div>
+);
 
-// ReactElement(Object) => HTML(Browser Understands)
-
-// const heading = React.createElement(
-//   "h1",
-//   { id: "heading", abc: "abc" },
-//   "Hello World from React !"
-// ); // Its a react element and returns an object
+const HeadingComponent = () => (
+  <div>
+    {jsxHeading}
+    {TitleComponent({ title: "Calling Functiion" })}
+    {/* This is also called Component Composition - A component inside another component is called Component Composition */}
+    <TitleComponent title={"Title"} />
+    <h1>Namaste React Heading Functional Component.</h1>
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(parent);
+root.render(<HeadingComponent />);
