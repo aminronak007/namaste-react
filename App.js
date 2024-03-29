b467e21aeb9b06542007eb8c084d17a88bd2bf80
Uss Dev 1 <ussdev1@unitesoftwaresolutions.com>
2024-03-29 11:10:47 +0530

    Episode 2 Completed

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child1" }, [
     React.createElement(
@@ -14,12 +17,12 @@ const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child2" }, [
     React.createElement(
       "h1",
-      { key: "h1", id: "heading" },
+      { key: "h1.", id: "heading" },
       "I'm a child2 h1 Tag"
     ),
     React.createElement(
       "h2",
-      { key: "h2", id: "heading" },
+      { key: "h2.", id: "heading" },
       "I'm a child2 h2 Tag"
     ),
   ]),
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Namaste React
+
+# Parcel
+- Dev build
+- Local server
+- HMR = Hot Module Replacement
+- It uses a File Watching Algorithms - written in C++
+- Caching - Faster builds
+- Image Optimization
+- Minification
+- Bundling
+- Compressing
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling - support older browsers
+- Diagnostics
+- Error Handling
+- Provides HTTPS url also
+- Tree Shaking - remove unused code
+- Different dev and prod bundles
